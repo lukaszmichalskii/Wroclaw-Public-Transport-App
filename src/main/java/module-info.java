e9cd4038f1b439cde1bb02e0c708285f1f31ad84
Łukasz Michalski <lukasz.michalski222@gmail.com>
2021-12-06 15:44:20 +0100

    Initial commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,15 @@
+module wpta.wroclawpublictransportapp {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+
+    opens wpta.wroclawpublictransportapp to javafx.fxml;
+    exports wpta.wroclawpublictransportapp;
+}
---
+++ src/main/java/wpta/wroclawpublictransportapp/AppController.java
@@ -0,0 +1,14 @@
+package wpta.wroclawpublictransportapp;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class AppController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+}
