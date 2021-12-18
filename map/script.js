c59db7f88eb0bb10431b4764212a918d6992c7e7
Łukasz Michalski <lukasz.michalski222@gmail.com>
2021-12-18 16:38:59 +0100

    Test cirlce drawing option

--- map/script.js
+++ map/script.js
@@ -1,8 +1,28 @@
+const citymap = {
+	newyork: {
+		center: { lat: 40.714, lng: -74.005 },
+		population: 8405837,
+	}
+}
+
 function initMap() {
 	var map = new google.maps.Map(document.getElementById('map'), {
 		zoom: 13,
 		center: {lat: 51.1078479035455, lng: 17.038266655466643}
 	});
+	for (const city in citymap) {
+		const cityCircle = new google.maps.Circle({
+			strokeColor: "#FF0000",
+			strokeOpacity: 0.8,
+			strokeWeight: 2,
+			fillColor: "#FF0000",
+			fillOpacity: 0.35,
+			map,
+			center: citymap[city].center,
+			radius: Math.sqrt(citymap[city].population) * 100,
+		});
+	}
+
 
 	var geocoder = new google.maps.Geocoder();
 
